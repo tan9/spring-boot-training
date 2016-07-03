58884a5eea1f466934b8e605e34dd2a5465f09cb
Pei-Tang Huang <tangtheone@gmail.com>
2016-07-03 23:09:59 +0800

    Add bootstrap class and sample controller.

---
+++ src/main/java/com/cht/inception/Application.java
@@ -0,0 +1,19 @@
+package com.cht.inception;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
