a6842b1aaf5cf7fc84ea862f1ec745629613357c
Pei-Tang Huang <tangtheone@gmail.com>
2016-07-05 02:19:46 +0800

    Added custom health indicator.

---
+++ src/main/java/com/cht/inception/configuration/ActuatorConfiguration.java
@@ -0,0 +1,15 @@
+package com.cht.inception.configuration;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorConfiguration {
+
+	@Bean
+	public HealthIndicator holyUpHealthIndicator() {
+		return () -> Health.up().build();
+	}
+}
