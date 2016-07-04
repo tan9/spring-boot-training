b89cdad764ef90a6c506ad8feca73ce91b2d5b25
Pei-Tang Huang <tangtheone@gmail.com>
2016-07-05 00:46:15 +0800

    Use thymeleaf template.

--- src/main/java/com/cht/inception/controller/HomeController.java
+++ src/main/java/com/cht/inception/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.cht.inception.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "home";
     }
 }
